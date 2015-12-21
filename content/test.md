dee90990943c0e5779d3c93fb38ad54fd4dcb8f5
CHANN <chann@chann.kr>
2015-12-22 01:11:05 +0900

    Add init conf and test docs.

---
+++ content/test.md
@@ -0,0 +1,8 @@
+Title: About
+Slug: about
+Date: 2015-12-25 12:25
+Category: Test
+Tags: Test
+Author: CHANN
+
+Test
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,9 +3,23 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'CHANN'
-SITENAME = 'Wiki'
-SITEURL = ''
+SITENAME = 'CHANN Wiki'
+SITESUBTITLE = 'Novice Programmer'
+# SITEURL = 'http://wiki.chann.kr'
+SITEURL = 'http://localhost:8000'
+# DISQUS_SITENAME = u'chann-wiki'
 
+# DISQUS_SECRET_KEY = u'YOUR_SECRET_KEY'
+# DISQUS_PUBLIC_KEY = u'YOUR_PUBLIC_KEY'
+
+### THEME SETTING
+# THEME = 'theme/notmyidea'
+# THEME = 'theme/replika'
+# BOOTSTRAP_NAVBAR_INVERSE = True
+# DISPLAY_CATEGORY_IN_BREADCRUMBS = True
+
+
+OUTPUT_PATH = 'output'
 PATH = 'content'
 
 TIMEZONE = 'Asia/Seoul'
@@ -20,16 +34,37 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+         ('Blog', 'https://blog.chann.kr'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+	('Resume', 'https://chann.kr'),
+	('Github', 'https://github.com/channprj'),
+	('Twitter', 'https://twitter.com/chann_kr'),
+	('Facebook', 'https://fb.com/channprj'),
+)
 
-DEFAULT_PAGINATION = 1
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Remove html extension
+ARCHIVE_URL = '{archive}'
+
+ARTICLE_URL = '{category}/{slug}'
+ARTICLE_SAVE_AS = ARTICLE_URL+'.html'
+
+PAGE_URL = '{slug}'
+PAGE_SAVE_AS = PAGE_URL+'.html'
+
+CATEGORY_URL = '{slug}/index'
+CATEGORY_SAVE_AS = CATEGORY_URL+'.html'
+
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = TAG_URL+'.html'
+
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = AUTHOR_URL+'.html'
